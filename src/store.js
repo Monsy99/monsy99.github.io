37aeaf48a01718e6d0ef658171528afb88feeffe
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-06 23:08:07 +0100

    added selectors to redux store and reducers for projects fetching

--- src/store.js
+++ src/store.js
@@ -4,14 +4,35 @@ const appSlice = createSlice({
   name: "app",
   initialState: {
     darkTheme: true,
+    projects: null,
+    loading: false,
   },
   reducers: {
-    switchTheme: ({ darkTheme }) => (darkTheme = !darkTheme),
+    switchTheme: (state) => {
+      state.darkTheme = !state.darkTheme;
+    },
+    startFetchProjects: (state) => {
+      state.loading = true;
+    },
+    fetchProjectsSuccess: (state, { payload }) => {
+      state.projects = payload;
+      state.loading = false;
+    },
   },
 });
 
 const store = configureStore({
   reducer: appSlice.reducer,
 });
-export const { switchTheme } = appSlice.actions;
+
+const selectProjects = (state) => state.projects;
+const selectDarkTheme = (state) => state.darkTheme;
+const selectLoading = (state) => state.loading;
+
+export { selectDarkTheme, selectProjects, selectLoading };
+export const {
+  switchTheme,
+  startFetchProjects,
+  fetchProjectsSuccess,
+} = appSlice.actions;
 export default store;
