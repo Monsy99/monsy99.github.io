ea85dc7eabd555b35cd9d11d689469b52477b001
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-14 15:42:11 +0100

    added github logo depending on theme

--- src/Homepage/Projects/index.js
+++ src/Homepage/Projects/index.js
@@ -1,11 +1,15 @@
 import { Section, GithubLogo, Header, Subheader } from "./styled";
 import ProjectsTiles from "./ProjectsTiles";
 import logo from "../../images/gh_logo.svg";
+import lightLogo from "../../images/gh_logo_light.svg";
+import { useSelector } from "react-redux";
+import { selectDarkTheme } from "../../theme/themeSlice";
 
 const Projects = () => {
+  const isDark = useSelector(selectDarkTheme);
   return (
     <Section>
-      <GithubLogo src={logo}></GithubLogo>
+      <GithubLogo src={isDark ? logo : lightLogo}></GithubLogo>
       <Header>Portfolio</Header>
       <Subheader>My recent projects</Subheader>
       <ProjectsTiles></ProjectsTiles>
