1f5b6ae3d890cfb3d8c1934946f8061561a9ade6
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-14 15:41:21 +0100

    added theme sections for each component

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,36 @@
 const theme = {
   color: {
-    white: "#ffffff",
-    mineSchaft: "#252525",
-    dodgerBlue: "#2188FF",
+    background: "#252525",
+    head: {
+      caption: "#FFFFFF",
+      name: "#FFFFFF",
+      description: "#FFFFFF",
+      buttonBackground: "#2188FF",
+      buttonText: "#FFFFFF",
+      toggleBackground: "#2188FF",
+    },
+    skillset: {
+      header: "#FFFFFF",
+      separator: "#E5E5E5",
+      item: "#FFFFFF",
+      background: "rgba(54, 54, 54, 0.72)",
+    },
+    projects: {
+      mainHeader: "#FFFFFF",
+      background: "rgba(54, 54, 54, 0.72)",
+      header: "#FFFFFF",
+      description: "#FFFFFF",
+      info: "#FFFFFF",
+      ref: "#2188FF",
+    },
+    contact: {
+      header: "#FFFFFF",
+      description: "#FFFFFF",
+      email: "#FFFFFF",
+    },
+    loader: {
+      header: "#FFFFFF",
+    },
   },
   breakpoint: {
     mobile: "767px",
@@ -11,9 +39,37 @@ const theme = {
 };
 const lightTheme = {
   color: {
-    white: "#ffffff",
-    mineSchaft: "#252525",
-    dodgerBlue: "#2188FF",
+    background: "#E5E5E5",
+    head: {
+      caption: "#6E7E91",
+      name: "#252525",
+      description: "#6E7E91",
+      buttonBackground: "#0366D6",
+      buttonText: "#FFFFFF",
+      toggleBackground: "#6E7E91",
+    },
+    skillset: {
+      header: "#252525",
+      separator: "rgba(209, 213, 218, 0.3)",
+      item: "#6E7E91",
+      background: "#FFFFFF",
+    },
+    projects: {
+      mainHeader: "#252525",
+      background: "#FFFFFF",
+      header: "#0366D6",
+      description: "#6E7E91",
+      info: "#6E7E91",
+      ref: "#0366D6",
+    },
+    contact: {
+      header: "#6E7E91",
+      description: "#252525",
+      email: "#252525",
+    },
+    loader: {
+      header: "#252525",
+    },
   },
   breakpoint: {
     mobile: "767px",
